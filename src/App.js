0452e8c1cbcd9f3a8c96fa1bc9864bbfddc9d78d
Ryan Mattox <509528@gmail.com>
2020-07-08 18:18:41 -0700

    "Added an image and canvas"

--- src/App.js
+++ src/App.js
@@ -4,13 +4,26 @@ import "./Style.css";
 class App extends Component {
     constructor(props) {
         super(props);
+
         this.image = new Image();
+        // Data url of a 1x1 pixel as placeholder image
         this.image.src = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAAC0lEQVQYV2NgAAIAAAUAAarVyFEAAAAASUVORK5CYII=";
+
         this.canvas = document.createElement("canvas");
         this.ctx = this.canvas.getContext('2d');
+
+        // Create a test character to get the width from
         this.testChar = document.createElement("pre");
+
+        // Set the canvas so that the text becomes the same size as the input image
         this.canvas.width = this.image.width / this.testChar.clientWidth;
         this.canvas.height = this.image.height / this.testChar.clientHeight;
+
+        this.state = {
+            columns: 80,
+            charWidth: this.testChar.clientWidth,
+            charHeight: this.testChar.clientHeight,
+        }
     }
     render() {
         return (
